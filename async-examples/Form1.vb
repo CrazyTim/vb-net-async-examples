e9a5703b743532bdc0bb3d5f6576228e214ee0dc
CrazyTim <impdeveloper@gmail.com>
2020-01-22 10:21:44 +1100

    Add async dialog code and reorganise files

--- async-examples/Form1.vb
+++ async-examples/Form1.vb
@@ -1,62 +1,23 @@
-﻿Imports System.Net.Http
-Imports System.Runtime.CompilerServices
+﻿Public Class Form1
 
-Public Class Form1
+    ' refer: https://docs.microsoft.com/en-us/dotnet/visual-basic/programming-guide/concepts/async/
 
+    Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-        TestAsync()
-
-    End Sub
-
-
-    ' 1) this is an example of a function that will block the GUI thread
-    Private Function SlowTaskFunction(ByVal p As String, ByVal Delay As Integer) As String
-
-        Dim sw As Stopwatch
-        sw = Stopwatch.StartNew()
-
-        Threading.Thread.Sleep(Delay)
-
-        Console.WriteLine($"SlowTaskFunction({p}): {sw.ElapsedMilliseconds}ms")
-
-        Return p
-
-    End Function
-
-
-    Private Sub SlowTaskSub(ByVal Delay As Integer)
-
-        Dim sw As Stopwatch
-        sw = Stopwatch.StartNew()
-
-        Threading.Thread.Sleep(Delay)
-
-        Console.WriteLine($"SlowTaskSub: {sw.ElapsedMilliseconds}ms")
+        Await Test_ShowAsyncDialog(Me)
+        Await Test_AsyncCalls()
+        Me.Close()
 
     End Sub
 
-
-    ' 2) this is how you build an aysnc wrapper for it
-    Private Function SlowTaskAsync(ByVal p As String, ByVal Delay As Integer) As Task(Of String)
-
-        Dim t As Task(Of String) = Task.Run(Function() SlowTaskFunction(p, Delay)) ' call the slow task
-
-        Return t
-
-    End Function
-
-
-    ' 3) testing async
-    Public Async Sub TestAsync()
+    Public Async Function Test_AsyncCalls() As Task
 
         ' nb: always wrap in try if in a sub, as it will not be caught and crash app
 
         Dim s As String = ""
 
         Try
-            s = Await SlowTaskAsync("1", 2000)
+            s = Await SlowFunctionAsyncWrapper("1", 2000)
         Catch ex As Exception
             Console.WriteLine($"1: exception: {ex.Message}")
         End Try
@@ -64,7 +25,7 @@ Public Class Form1
         Application.DoEvents()
 
         Try
-            s = Await SlowTaskAsync("2", 2500).TimeoutAfter(2000) ' this will timeout!!
+            s = Await SlowFunctionAsyncWrapper("2", 2500).TimeoutAfter(2000) ' this will timeout!!
         Catch ex As Exception ' nb: catch timeout here
             Console.WriteLine($"2: exception: {ex.Message}")
         End Try
@@ -72,7 +33,7 @@ Public Class Form1
         Application.DoEvents() ' nb: need do events here because textbox won't have enough time to update before the GUI thread is frozen when we call AccessTheWeb
 
         Try
-            s = SlowTaskAsync("3", 2000).Result ' this task will execute syncronously and block the GUI thread
+            s = SlowFunctionAsyncWrapper("3", 2000).Result ' this task will execute syncronously and block the GUI thread
         Catch ex As Exception
             Console.WriteLine($"3: exception: {ex.Message}")
         End Try
@@ -80,7 +41,7 @@ Public Class Form1
         Application.DoEvents()
 
         Try
-            s = Await SlowTaskAsync("4", 1500)
+            s = Await SlowFunctionAsyncWrapper("4", 1500)
         Catch ex As Exception
             Console.WriteLine($"4: exception: {ex.Message}")
         End Try
@@ -88,48 +49,31 @@ Public Class Form1
         Application.DoEvents()
 
 
-        ' await a slow sub
-        ' no way to timeout sub atm :(
+        ' workaround if you want to await a sub (but really you should always use a function):
         Try
-            Await Task.Run(Sub() SlowTaskSub(1500)) ' call the slow task
+            Await Task.Run(Sub() SlowSub("5", 1500)) ' call the slow task
         Catch ex As Exception
             Console.WriteLine($"sub: exception: {ex.Message}")
         End Try
 
-    End Sub
-
-
-End Class
-
-
-Public Module test
-
-
-    ' this very useful extension allows you to put a timeout on a task
-    <Extension()>
-    Public Async Function TimeoutAfter(Of TResult)(ByVal task As Task(Of TResult), ByVal TimeoutMilliseconds As Long) As Task(Of TResult)
-
-        ' adapted from: https://stackoverflow.com/a/22078975/737393
-
-        Using timeoutCancellationTokenSource = New Threading.CancellationTokenSource()
-
-            Dim completedTask
-
-            If TimeoutMilliseconds = 0 Then
-                completedTask = Await task.WhenAny(task)
-            Else
-                completedTask = Await task.WhenAny(task, task.Delay(New TimeSpan(TimeoutMilliseconds * TimeSpan.TicksPerMillisecond), timeoutCancellationTokenSource.Token))
-            End If
-
-            If completedTask Is task Then
-                timeoutCancellationTokenSource.Cancel()
-                Return Await task
-            Else
-                Throw New TimeoutException()
-            End If
-
-        End Using
     End Function
 
+    Public Async Function Test_ShowAsyncDialog(ByVal ParentForm As Form) As Task
 
-End Module
+        Using f As New Form() With {
+                .Text = "Loading...",
+                .Width = 300,
+                .Height = 100,
+                .StartPosition = FormStartPosition.CenterParent
+            }
+
+            ParentForm.BeginInvoke(Sub() f.ShowDialog(ParentForm)) ' show non-blocking dialog
+
+            Await SlowAsyncFunction("1", 2000)
+
+            f.Close()
+        End Using
+
+    End Function
+
+End Class
---
+++ async-examples/Module1.vb
@@ -0,0 +1,78 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module Module1
+
+    ''' <summary> Set a timeout for the task. A TimeoutException will be raised. </summary>
+    <Extension()>
+    Public Async Function TimeoutAfter(Of TResult)(ByVal task As Task(Of TResult), ByVal TimeoutMilliseconds As Long) As Task(Of TResult)
+
+        ' adapted from: https://stackoverflow.com/a/22078975/737393
+
+        Using timeoutCancellationTokenSource = New Threading.CancellationTokenSource()
+
+            Dim completedTask
+
+            If TimeoutMilliseconds = 0 Then
+                completedTask = Await task.WhenAny(task)
+            Else
+                completedTask = Await task.WhenAny(task, task.Delay(New TimeSpan(TimeoutMilliseconds * TimeSpan.TicksPerMillisecond), timeoutCancellationTokenSource.Token))
+            End If
+
+            If completedTask Is task Then
+                timeoutCancellationTokenSource.Cancel()
+                Return Await task
+            Else
+                Throw New TimeoutException()
+            End If
+
+        End Using
+    End Function
+
+    ' if awaited, this function will not block the gui.
+    Public Async Function SlowAsyncFunction(ByVal p As String, ByVal Delay As Integer) As Task
+
+        Dim sw As Stopwatch
+        sw = Stopwatch.StartNew()
+
+        Await Task.Delay(Delay)
+
+        Console.WriteLine($"{p}: {sw.ElapsedMilliseconds}ms")
+
+    End Function
+
+    ' this function that will block the gui
+    Public Function SlowFunction(ByVal p As String, ByVal Delay As Integer) As String
+
+        Dim sw As Stopwatch
+        sw = Stopwatch.StartNew()
+
+        Threading.Thread.Sleep(Delay)
+
+        Console.WriteLine($"{p}: {sw.ElapsedMilliseconds}ms")
+
+        Return p
+
+    End Function
+
+    ' this sub that will block the gui
+    Public Sub SlowSub(ByVal p As String, ByVal Delay As Integer)
+
+        Dim sw As Stopwatch
+        sw = Stopwatch.StartNew()
+
+        Threading.Thread.Sleep(Delay)
+
+        Console.WriteLine($"{p}: {sw.ElapsedMilliseconds}ms")
+
+    End Sub
+
+    ' this is how you build an async wrapper for a slow task
+    Public Function SlowFunctionAsyncWrapper(ByVal p As String, ByVal Delay As Integer) As Task(Of String)
+
+        Dim t As Task(Of String) = Task.Run(Function() SlowFunction(p, Delay)) ' call the slow task
+
+        Return t
+
+    End Function
+
+End Module
